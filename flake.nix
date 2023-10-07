b2774b96ced25d6f116718d4cc6d696935f4ac08
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-10-06 19:09:03 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,149 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    fishnet = {
+      url = "git+https://github.com/lichess-org/fishnet?submodules=1";
+      flake = false;
+    };
+
+    nnueNet = {
+      url = "https://tests.stockfishchess.org/api/nn/nn-5af11540bbfe.nnue";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    naersk,
+    fishnet,
+    nnueNet,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forEachSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems
+      (system:
+        f {
+          inherit system;
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    containers = forEachSystem ({
+      system,
+      pkgs,
+    }: {
+      autoStart = true;
+      privateNetwork = true;
+      hostAddress = "192.168.100.10";
+      localAddress = "192.168.100.11";
+      hostAddress6 = "fc00::1";
+      localAddress6 = "fc00::2";
+      config = {
+        config,
+        lib,
+        ...
+      }: {
+        imports = [self.nixosModules.${system}.default];
+
+        services.fishnet = {
+          enable = true;
+          keyFile = pkgs.writeTextFile "fishnet.key" "invalid";
+        };
+
+        nix.settings.experimental-features = ["nix-command"];
+
+        services.resolved.enable = true;
+      };
+    });
+    nixosModules = forEachSystem ({
+      system,
+      pkgs,
+    }: {
+      default = {
+        config,
+        lib,
+        ...
+      }: let
+        inherit (lib) types mkOption mkEnableOption mkIf;
+        cfg = config.services.fishnet;
+      in {
+        options.services.fishnet = {
+          enable = mkEnableOption "Fishnet";
+          keyFile = mkOption {
+            description = "File that contains your Fishnet key.";
+            type = types.path;
+          };
+          cores = mkOption {
+            description = "The number of CPU cores to use";
+            type = types.number;
+            default = 3;
+          };
+          joinEvenIfNoBacklog = mkOption {
+            description = ''
+              Join even if no backlog of analyses needed.
+              Set to false for laptops.
+            '';
+            type = types.bool;
+            default = true;
+          };
+        };
+        config = mkIf cfg.enable {
+          systemd.services.fishnet = {
+            description = "Distributed Stockfish analysis for lichess.org";
+            wantedBy = ["multi-user.target"];
+            after = ["network.target"];
+            serviceConfig = {
+              User = "fishnet";
+              ExecStart = pkgs.writeShellScript "fishnet-launcher" ''
+                ${self.packages.${system}.fishnet}/bin/fishnet \
+                  --no-conf \
+                  --key-file ${cfg.keyFile} \
+                  --cores ${cfg.cores} \
+                  --user-backlog ${
+                  if cfg.joinEvenIfNoBacklog
+                  then 0
+                  else "short"
+                } \
+                  --system-backlog ${
+                  if cfg.joinEvenIfNoBacklog
+                  then 0
+                  else "long"
+                }
+              '';
+            };
+          };
+        };
+      };
+    });
+    packages =
+      forEachSystem
+      ({
+        system,
+        pkgs,
+      }: let
+        naerskLib = pkgs.callPackage naersk {};
+      in {
+        default = naerskLib.buildPackage {
+          src = fishnet;
+          overrideMain = p: {
+            patchPhase = ''
+              cp ${nnueNet} Stockfish/src/nn-5af11540bbfe.nnue
+            '';
+          };
+        };
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+            ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+      });
+  };
+}
